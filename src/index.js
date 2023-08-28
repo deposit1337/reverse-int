555efc4ca0237f64428528b586d00bf55bc34deb
Pavel Chernykh <propopa228@mail.ru>
2023-08-28 13:07:15 +0800

    add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    
+    const negative = n < 0;
+    let str = Math.abs(n).toString();
+    let reversedString = str.split('').reverse().join('')
+    let reversedNum = parseInt(reversedString)
+    return reversedNum;
+
 }
